00658e4637fdd77543132036a8ab96117e409124
Sylvain Boissel <sylvain@ashtree.eu>
2014-11-02 17:33:42 +0100

    Details

--- communes.php
+++ communes.php
@@ -15,8 +15,9 @@ $language = isset($_REQUEST['language']) ? $_REQUEST['language'] : 'fr';
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>WDUtils | Tool Labs</title>
-	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-	<script src="../bootstrap/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	<script src="js/sorttable.js"></script>
 	<style>
 .navbar-logo img {
