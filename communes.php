64fde1e91c04db61b23bd02239223c12108fbc45
Sylvain Boissel <sylvain@ashtree.eu>
2014-11-02 14:26:59 +0100

    First commit

---
+++ communes.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Queries wikidata items without label in a specified language.
+ * Released under BSD license.
+ */
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Communes françaises | Tool Labs</title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="/wikidata-nolabels/js/sorttable.js"></script>
+	<style>
+.navbar-logo img {
+	width: 32px;
+	padding-top: 9px !important;
+	padding-bottom: 0 !important;
+}
+
+.navbar-logo a {
+	padding-top: 0 !important;
+	padding-bottom: 0 !important;
+}
+
+table {
+	border-spacing: 10px;
+	border-collapse: separate;
+}
+
+table.sortable th:not(.sorttable_sorted):not(.sorttable_sorted_reverse):not(.sorttable_nosort):after {
+	content: " \25B4\25BE";
+}
+	</style>
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="/wikidata-nolabels/">Communes de France</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="/wikidata-nolabels/">Query</a></li>
+      <li><a href="/wikidata-nolabels/help.html">Help</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+        <li class="navbar-logo"><a href="https://tools.wmflabs.org"><img title="Powered by Wikimedia Labs" src="//upload.wikimedia.org/wikipedia/commons/thumb/6/60/Wikimedia_labs_logo.svg/32px-Wikimedia_labs_logo.svg.png" /></a></li>
+        <li class="navbar-logo"><a href="http://ashtree.eu/"><img title="Developed by Sylvain Boissel" src="//ashtree.eu/avatars/logo2-32.png" /></a></li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
+<div id="main_content" class="container"><div class="row">
+<?php
+	require_once('lib/WDQCleanedResults.php');
+	
+	try {
+			$communesQuery = new WDQCleanedResults("claim[31:484170]","fr");
+			$communesQuery->run();
+			
+			echo "<pre>";
+			print_r($communesQuery);
+			echo "</pre>";
+	} catch (Exception $ex) {
+		echo "<p>", $ex->getMessage(), "</p>";
+	}
+?>
+
---
+++ lib/WDQCleanedResults.php
@@ -0,0 +1,124 @@
+<?php
+require_once('ReplicationDatabase.php');
+
+class WDQCleanedResults {
+	/// Makes a WDQ query and returns cleaned results as a PHP object
+	
+	///
+	/// Properties
+	///
+
+	/**
+	 * Query results
+	 * @var array
+	 */
+	public $results;
+
+	/**
+	 * Query
+	 * @var string
+	 */
+	public $query;
+	
+	/**
+	 * Language
+	 * @var string
+	 */
+	public $language;
+	
+	/**
+	 * Initializes a new instance of the WikidataNoLabelsQuery class
+	 *
+	 * @param string $query The WDQ query to run
+	 * @param string $language The language to select items without labels in
+	 */
+	function __construct ($query, $language) {
+		$this->query = $query;
+		$this->language = $language;
+	}
+
+	///
+	/// Query logic
+	///
+
+	/**
+	 * Runs the queries
+	 *
+	 * After this method has ben called, $this->results is populated.
+	 */
+	function run () {
+		if (!self::isValidWDQ($this->query)) {
+			throw new Exception("WDQ isn't valid.");
+		}
+		
+		// Get the items list from WDQ
+		$items = self::queryWDQ($this->query);
+		
+		$labels = $this->getItemsLabels($this->language, $items);
+		foreach ($labels as $label) {
+			$key = $label['id'];
+			$this->results[$key] = $label['label'];
+		}		
+	}
+	
+	///
+	/// Replication databases label information helper methods
+	///
+
+	/**
+	 * Get the entities labels
+	 *
+	 * @param string $language the language to gets labels defined in
+	 * @param string $itemsHaystack the items haystack
+	 * @param bool $queryLabels queries also the labels
+	 * @return array
+	 */
+	function getItemsLabels ($language, $itemsHaystack) {
+		//TODO: sanitize the language, should be a valid language code
+		//TODO: sanitize the haystack, should only contain numeric id
+		$clauseIn = join(', ', $itemsHaystack);
+		$sql = "SELECT term_entity_id, term_text
+			FROM wb_terms
+		        WHERE term_type = 'label' AND term_language = '$language' AND term_entity_id IN ($clauseIn)";
+
+		$db = ReplicationDatabaseFactory::get('wikidatawiki');
+		$result = $db->query($sql);
+		$items = array();
+		while ($row = $result->fetch_assoc()) {
+			$items[] = array(
+				'id' => $row['term_entity_id'],
+				'label' => $row['term_text']
+			);
+		}
+		return $items;
+	}
+
+	///
+	/// WDQ helper methods
+	///
+
+	/**
+	 * Determines if the specified query is a valid one.
+	 *
+	 * @param string $query the WDQ
+	 * @return bool true if the query is valid; otherwise, false.
+	 */
+	static function isValidWDQ ($query) {
+		return true;
+	}
+
+	/**
+	 * Queries the WDQ server
+	 *
+	 * @param string $query the WDQ
+	 * @return array WDQ result
+	 */
+	static function queryWDQ ($query) {
+		$url = 'http://wdq.wmflabs.org/api?q=' . urlencode($query);
+		$data = json_decode(file_get_contents($url));
+		if ($data->status->error != 'OK') {
+			throw new Exception($data->status->error);
+		}
+		return $data->items;
+	}
+}
