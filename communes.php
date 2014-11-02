5f69635402371f74855ee0f7785f5f547cd8e7e9
Sylvain Boissel <sylvain@ashtree.eu>
2014-11-02 17:35:18 +0100

    Details

--- communes.php
+++ communes.php
@@ -17,6 +17,7 @@ $language = isset($_REQUEST['language']) ? $_REQUEST['language'] : 'fr';
 	<title>WDUtils | Tool Labs</title>
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	<script src="js/sorttable.js"></script>
 	<style>
