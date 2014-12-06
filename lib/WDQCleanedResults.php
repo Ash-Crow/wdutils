4945febc44025064a6b729391a0fa4b109cad5f3
Sylvain Boissel <sylvain@ashtree.eu>
2014-12-06 17:42:17 +0100

    eglises

--- lib/WDQCleanedResults.php
+++ lib/WDQCleanedResults.php
@@ -106,8 +106,8 @@ class WDQCleanedResults {
 			$url = 'https://www.wikidata.org/w/api.php?action=wbgetclaims&entity='.$item.'&property=P625&format=json';
 
 			$data = json_decode(file_get_contents($url));
-			$latitude = $data['claims']['P625']['mainsnak']['datavalue']['value']['latitude'];
-			$longitude = $data['claims']['P625']['mainsnak']['datavalue']['value']['longitude'];
+			$latitude = $data->claims->P625->mainsnak->datavalue->value->latitude;
+			$longitude = $data->claims->P625->mainsnak->datavalue->value->longitude;
 
 			$return = array($latitude,$longitude);
 		} else {
